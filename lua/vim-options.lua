e5674fe6338811d1ca2f1cf4182420c2f696da17
NicoOhR <nimoohayonr@gmail.com>
2024-07-23 20:37:34 -0500

    added trouble

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -16,3 +16,12 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
