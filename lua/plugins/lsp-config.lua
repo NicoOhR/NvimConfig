8566b0b56e24782b596cdcea21facf9b12791c0e
NicoOhR <nimoohayonr@gmail.com>
2024-08-04 00:38:21 -0500

    virtual text stuff, not ideal

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,51 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "clangd" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			setup = {
-				rust_analyzer = function()
-					return true
-				end,
-			},
-		},
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.rust_analyzer.setup({
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({
         capabilities = capabilities,
       })
-      vim.lsp.inlay_hint.enable(true, {0})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gt", vim.lsp.buf.definition, {})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+      vim.lsp.inlay_hint.enable(true, { 0 })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gt", vim.lsp.buf.definition, {})
       vim.diagnostic.config({
-      virtual_text = true,
+        virtual_text = true,
       })
       vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+    end,
+  },
+
 }
